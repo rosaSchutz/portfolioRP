57a6e28476c52b44624c83d042021a4e5cd69a09
rosaSchutz <rosschutz1997@gmail.com>
2023-10-26 00:05:49 -0500

    Feature language

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [astroI18next(), tailwind()],
+});
